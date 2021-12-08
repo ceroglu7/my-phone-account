47d6fc531ebc9d30e78b1f4bf4a6fe35f2226a1f
Cihan Eroğlu <ceroglu7@gmail.com>
2021-12-08 14:10:45 +0300

    LinQ

--- MyPhoneAccount/MainForm.cs
+++ MyPhoneAccount/MainForm.cs
@@ -5,6 +5,9 @@ using System.Windows.Forms;
 using System.IO;
 using FluentValidation;
 using Microsoft.Azure.Amqp.Framing;
+using System.Text.RegularExpressions;
+using System.Linq;
+using MyPhoneAccount;
 
 namespace MyPhoneAccount
 {
@@ -32,7 +35,7 @@ namespace MyPhoneAccount
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listView1.SelectedItems.Count == 0)
+            if (lstvResult.SelectedItems.Count == 0)
             {
                 lblNoPerson.Text = "Kayıt Seçilmeli";
                 lblFullName.Text = null;
@@ -43,26 +46,26 @@ namespace MyPhoneAccount
             }
             else
             {
-                var item = listView1.SelectedItems[0].Index;
+                var item = lstvResult.SelectedItems[0].Index;
                 string path = @"persons.json";
                 string PersonText = File.ReadAllText(path);
                 _persons = JsonConvert.DeserializeObject<List<PersonDto.Person>>(PersonText);
                 lblFullName.Text = "Ad Soyad : " + _persons[item].Fullname;
-                lblCompanyName.Text = "Firma Adı : " +  _persons[item].CompanyName;
+                lblCompanyName.Text = "Firma Adı : " + _persons[item].CompanyName;
                 lblGSM.Text = "GSM No : " + _persons[item].GSM;
-                lblPhone.Text = "Sabit Telefon : " +  _persons[item].Phone;
-                lblMail.Text = "E-Mail Adresi : " +  _persons[item].Email;
+                lblPhone.Text = "Sabit Telefon : " + _persons[item].Phone;
+                lblMail.Text = "E-Mail Adresi : " + _persons[item].Email;
                 lblNoPerson.Text = null;
             }
         }
         private void MainForm_Load(object sender, EventArgs e)
         {
-            listView1.Clear();
-            listView1.View = View.Details;
-            listView1.GridLines = true;
-            listView1.FullRowSelect = true;
-            listView1.Columns.Add("Ad Soyad");
-            listView1.Columns.Add("GSM");
+            lstvResult.Clear();
+            lstvResult.View = View.Details;
+            lstvResult.GridLines = true;
+            lstvResult.FullRowSelect = true;
+            lstvResult.Columns.Add("Ad Soyad");
+            lstvResult.Columns.Add("GSM");
 
             lblNoPerson.Text = "Kayıt Seçilmeli";
             lblFullName.Text = null;
@@ -75,24 +78,24 @@ namespace MyPhoneAccount
             string path = @"persons.json";
             if (!File.Exists(path))
             {
-                listView1.Items.Add("Kişi yada Firma Eklenmemiş");
+                lstvResult.Items.Add("Kişi yada Firma Eklenmemiş");
             }
             string PersonText = File.ReadAllText(path);
             _persons = JsonConvert.DeserializeObject<List<PersonDto.Person>>(PersonText);
 
-            
-            
-            
-            if (_persons==null)
+
+
+
+            if (_persons == null)
             {
                 _persons = new List<PersonDto.Person>();
-                listView1.Items.Add("Kişi yada Firma Eklenmemiş");
+                lstvResult.Items.Add("Kişi yada Firma Eklenmemiş");
             }
             else
             {
                 foreach (var item in _persons)
                 {
-                    listView1.Items.Add(item.Fullname);
+                    lstvResult.Items.Add(item.Fullname);
                 }
             }
 
@@ -100,9 +103,9 @@ namespace MyPhoneAccount
 
         private void btnDeletePerson_Click(object sender, EventArgs e)
         {
-            if (listView1.SelectedItems.Count > 0)
+            if (lstvResult.SelectedItems.Count > 0)
             {
-                var item = listView1.SelectedItems[0].Index;
+                var item = lstvResult.SelectedItems[0].Index;
                 //rest of your logic
                 _persons.RemoveAt(item);
                 //_persons.Remove(_persons[item]);
@@ -122,22 +125,95 @@ namespace MyPhoneAccount
         }
         private void RefreshListView()
         {
-            listView1.Items.Clear();
+            lstvResult.Items.Clear();
             foreach (var item in _persons)
             {
                 string[] row = { item.Fullname, item.GSM };
                 var listViewItem = new ListViewItem(row);
-                listView1.Items.Add(listViewItem);
+                lstvResult.Items.Add(listViewItem);
             }
         }
+        //
+
+
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            List<PersonDto.Person> _persons = new List<PersonDto.Person>();
+            string path = "persons.json";
+            string PersonText = File.ReadAllText(path);
+            _persons = JsonConvert.DeserializeObject<List<PersonDto.Person>>(PersonText);
+            string searchingText = txtSearch.Text;
+            if (cmbSearchCategory.SelectedItem == "Ad Soyad")
+            {
+                var searchResult = from item in _persons
+                                   where item.Fullname.Contains(searchingText)
+                                   select new { item.Fullname };
+                lstvResult.Items.Clear();
+                foreach (var result in searchResult)
+                {
+                    lstvResult.Items.Add(Convert.ToString(result));
+                }
+            }
+            else if (cmbSearchCategory.SelectedItem == "Şirket Adı")
+            {
+                var searchResult = from item in _persons
+                                   where item.CompanyName.Contains(searchingText)
+                                   select new { item.CompanyName };
+                lstvResult.Items.Clear();
+                foreach (var result in searchResult)
+                {
+                    lstvResult.Items.Add(Convert.ToString(result));
+                }
+            }
+            else if (cmbSearchCategory.SelectedItem == "GSM Numarası")
+            {
+                var searchResult = from item in _persons
+                                   where item.GSM.Contains(searchingText)
+                                   select new { item.GSM };
+                lstvResult.Items.Clear();
+                foreach (var result in searchResult)
+                {
+                    lstvResult.Items.Add(Convert.ToString(result));
+                }
+            }
+            else if (cmbSearchCategory.SelectedItem == "Sabit Tel.")
+            {
+                var searchResult = from item in _persons
+                                   where item.Phone.Contains(searchingText)
+                                   select new { item.Phone };
+                lstvResult.Items.Clear();
+                foreach (var result in searchResult)
+                {
+                    lstvResult.Items.Add(Convert.ToString(result));
+                }
+            }
+            else if (cmbSearchCategory.SelectedItem == "E-Mail")
+            {
+                var searchResult = from item in _persons
+                                   where item.Email.Contains(searchingText)
+                                   select new { item.Email };
+                lstvResult.Items.Clear();
+                foreach (var result in searchResult)
+                {
+                    lstvResult.Items.Add(Convert.ToString(result));
+                }
+            }
+            else
+                RefreshListView();
+            }
+        
+        }
+
     }
     public class PersonDtoValidator : AbstractValidator<PersonDto.Person>
     {
         public PersonDtoValidator()
         {
-            RuleFor(c => c.Email).EmailAddress().WithMessage("Mail bilgisi hatalı girildi").When(i=>!string.IsNullOrEmpty(i.Email));
-            RuleFor(c => c.Fullname).Length(3, 20).NotEmpty().NotNull().WithMessage("Ad Soyad bilgisi hatalı") ;
+            RuleFor(c => c.Email).EmailAddress().WithMessage("Mail bilgisi hatalı girildi").When(i => !string.IsNullOrEmpty(i.Email));
+            RuleFor(c => c.Fullname).Length(3, 20).NotEmpty().NotNull().WithMessage("Ad Soyad bilgisi hatalı");
             RuleFor(c => c.GSM).Length(11).NotNull().NotEmpty().WithMessage("GSM Hatalı");
         }
+
+
     }
-}
